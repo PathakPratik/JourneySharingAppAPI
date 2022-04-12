1fdaca8608ce2f390821da85aef0c94fab76f91a
sheilkumar <sk17@illinois.edu>
2022-04-12 19:32:16 +0100

    Added loadbalancer on 0.0.0.0:8000, listens to and redirects all traffic to 172.21.0.6:5000 (flask)

---
+++ LoadBalancer/main.go
@@ -0,0 +1,35 @@
+package main
+
+import(
+	"github.com/sheilkumar/LoadBalancerGo/loadbalancer"
+	"github.com/sheilkumar/LoadBalancerGo/server"
+	"fmt"
+	"net/http"
+	"log"
+	"time"
+	"github.com/gorilla/mux"
+// )
+)
+
+func main() {
+	lb := loadbalancer.LoadBalancer{}
+	r := mux.NewRouter()
+	// fmt.Println(lb)
+	// fmt.Println(sv1)
+	lb.AddServer(server.CreateNewServer("server-2", "http://172.21.0.6:5000"))
+	// lb.AddServer(server.CreateNewServer("server-3", "http://0.0.0.0:5000"))
+	// lb.AddServer(server.CreateNewServer("server-4", "http://0.0.0.0:5000"))
+	// lb.AddServer(server.CreateNewServer("server-5", "http://0.0.0.0:5000"))
+	fmt.Println(lb)
+	r.PathPrefix("/").HandlerFunc(lb.ServeRequest)
+	// http.HandleFunc("/", lb.ServeRequest)
+	srv := &http.Server{
+		Handler: r,
+		Addr:    "0.0.0.0:8000",
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+}
