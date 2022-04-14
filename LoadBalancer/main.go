d39d16d5d4bee53fc4fac1ee76df0022f98d0ef4
sheilkumar <sk17@illinois.edu>
2022-04-14 14:06:54 +0100

    Ensuring cohrence between different modules

--- LoadBalancer/main.go
+++ LoadBalancer/main.go
@@ -18,7 +18,8 @@ func main() {
 	r := mux.NewRouter()
 	// fmt.Println(lb)
 	// fmt.Println(sv1)
-	for i:=1; i<5; i++ {
+	lb.AddServer(server.CreateNewServer("server-1","http://api:5000"))
+	for 2:=1; i<5; i++ {
 		lb.AddServer(server.CreateNewServer("server-"+strconv.Itoa(i), "http://api"+strconv.Itoa(i)+":5000"))
 	}
 	// lb.AddServer(server.CreateNewServer("server-2", "http://api:5000"))
